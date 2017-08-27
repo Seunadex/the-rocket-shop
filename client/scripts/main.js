af00e9de5722c497265a832ae6770f8bc3cfd3a3
seunadex <seun.adekunle@andela.com>
2017-08-27 11:56:20 +0100

    feature(route): setup route using iron router

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,21 +1,21 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../templates/app/layout.html';
+import '../../client/templates/app/layout.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.layout.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.layout.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
+Template.layout.events({
+  'click button': function (event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
